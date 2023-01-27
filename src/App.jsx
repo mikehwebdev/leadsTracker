d079bf8d439a8ae16a48c7998fe2fbe8e040ae31
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-01-27 15:13:42 +0000

    Final bundling and deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
+  const [links, setLinks] = useState(JSON.parse(localStorage.getItem("links"))?? [])
+  const [inputValue, setInputValue] = useState('')
   
+  
+function addressUpdater(e){
+    setInputValue(e.target.value)
+}
+
+function submitAddress(){
+  setLinks(
+    prev => ([...links, <li key={inputValue}><a href={`https://${inputValue}`}>{inputValue}</a></li>])
+  )
+  setInputValue('')
+  useEffect(()=>{
+    localStorage.setItem("links", JSON.stringify(links))
+  }, [links])
+}
+
+function deleteLeads(){
+  setLinks([])
+}
+
+
+
+
   return (
     <div className="app">
-      <h1>Test</h1>
+        <input type="text" 
+        placeholder="type address here..." 
+        onChange={addressUpdater}
+        value={inputValue}>
+        </input>
+        <button className="btn save-input" onClick={submitAddress}>Save input</button>
+        <button className="btn delete" onClick={deleteLeads}>Delete all</button>
+        <ul>{links}</ul>
     </div>
   )
 }
